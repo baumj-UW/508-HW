2fdaf00e5ff4b5ede317989f1b3c906d2618f337
baumj-UW <45178573+baumj-UW@users.noreply.github.com>
2019-05-08 17:28:16 -0700

    added general test

--- MarkovChainGivenSS.py
+++ MarkovChainGivenSS.py
@@ -32,7 +32,18 @@ b = np.zeros((6,1))
 b[5,0] = 1
 px = lstsq(M,b,rcond=None)[0]
 
-print(px)
+#print(px)
+
+## general functions## 
+
+def SolveSS(P):
+    #Solve for stationary probabilities given P matrix
+    J = np.eye(P.shape[0],P.shape[1]) - P.T
+    M = np.insert(J,P.shape[0],1,axis=0)
+    b = np.zeros((P.shape[0]+1,1))
+    b[P.shape[0],0] = 1
+    px = lstsq(M,b,rcond=None)[0]
+    return px[:,0]
 
 def MakeP(ss):
     ## generate P transition matrix given transient probabilities
@@ -48,6 +59,47 @@ def MakeP(ss):
     return P
 
 test = MakeP(ss[0])
+check = SolveSS(test)
 
 
-print(test)
+print("Generated P:",test)
+print("Check stationary probabilities:",check)
+print("original",ss[0])
+
+test1 = np.array([[1/4, 1/4, 1/4, 1/4]])
+test2 = np.array([[5/30, 10/30, 7/30, 3/30, 5/30]])
+test3 = np.array([[2/10, 5/10, 3/10]])
+test4 = np.array([[1/2, 1/4, 1/8, 1/16, 1/16]])
+test5 = np.array([[3/25, 3/25, 5/25, 5/25, 9/25]])
+
+print("Generalized Test:")
+res1 = MakeP(test1[0])
+check1 = SolveSS(res1)
+print("Generated P:",res1)
+print("Check stationary probabilities:",check1)
+print("original",test1[0])
+print("************************")
+res2 = MakeP(test2[0])
+check2 = SolveSS(res2)
+print("Generated P:",res2)
+print("Check stationary probabilities:",check2)
+print("original",test2[0])
+print("************************")
+res3 = MakeP(test3[0])
+check3 = SolveSS(res3)
+print("Generated P:",res3)
+print("Check stationary probabilities:",check3)
+print("original",test3[0])
+print("************************")
+res4 = MakeP(test4[0])
+check4 = SolveSS(res4)
+print("Generated P:",res4)
+print("Check stationary probabilities:",check4)
+print("original",test4[0])
+print("************************")
+res5 = MakeP(test5[0])
+check5 = SolveSS(res5)
+print("Generated P:",res5)
+print("Check stationary probabilities:",check5)
+print("original",test5[0])
+print("************************")
