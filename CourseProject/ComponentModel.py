428c0c02f3b2e1cd9b91a689a971a66103115f2f
baumj-UW <45178573+baumj-UW@users.noreply.github.com>
2019-05-30 21:51:26 -0700

    Added basic system model that uses iid component model. plots reliability over 50yr

--- CourseProject/ComponentModel.py
+++ CourseProject/ComponentModel.py
@@ -98,23 +98,5 @@ plt.ylabel('Prob of being in state')
 plt.legend()
 plt.title("Component State probabilities over time")
 
-
-
-##reliability function
-#try 3 components in parallel + 1 in series
-P = np.zeros((4,len(results.t)))
-for comp in range(4):
-    P[comp] = results.y[3] #prob that device is in fail state (consider including other states like maintenance?)
-
-#make sure this is elementwise multiplication
-R = (1-P[0])*(1-(P[1])*(P[2])*(P[3])) #from Ross Reliability chapter, p(working)=1-P(fail) for each component 
-
-relfig = plt.figure(2)
-plt.plot(results.t,R)
-plt.grid(True)
-plt.xlabel('Time (days)')
-plt.ylabel('Prob of system failure')
-plt.title("System Reliability")
-
-plt.show()
+#plt.show()  UNCOMMENT TO SHOW PLOT  HERE
 print('working?')
