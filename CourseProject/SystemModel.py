ed7635617eae5bfbe426817d99071c583cad4e52
Lane Smith <lanesmith218@gmail.com>
2019-06-02 19:02:40 -0700

    Added Mean Time to Failure (MTTF)
    Added the MTTF calculation for both an individual component and the whole system. To determine MTTF, the failure state has to become an absorbing state; the new .csv with abs in the title contains the values of the new Q matrix. Also, for the MTTF calculation, you need to have a much longer period. I found that 300 years is pretty accurate, while 100 years gives a good idea of the MTTF that we want.

--- CourseProject/SystemModel.py
+++ CourseProject/SystemModel.py
@@ -23,11 +23,38 @@ for comp in range(4):
 #make sure this is elementwise multiplication
 R = (1-P[0])*(1-(P[1])*(P[2])*(P[3])) #from Ross Reliability chapter, p(working)=1-P(fail) for each component 
 
+#Determine the mean time to failure (MTTF) of the system
+MTTF_array = []
+for i in range(len(R)):
+    if i > 0:
+        #Compute MTTF using numerical integration (trapezoidal rule) for each segment
+        MTTF_elem = (1/2)*(R[i] + R[i - 1])*(results.t[i] - results.t[i - 1])
+        MTTF_array.append(MTTF_elem)
+
+#Sum all the numerically-integrated segments for the total MTTF
+MTTF = sum(MTTF_array)/365
+print("The mean time to failure is %.2f years.\n" % MTTF)
+
+#Reliability of one component
+R1 = (1 - P[0])
+
+#Determine the mean time to failure (MTTF) of a single element
+MTTF_array1 = []
+for i in range(len(R1)):
+    if i > 0:
+        #Compute MTTF using numerical integration (trapezoidal rule) for each segment
+        MTTF_elem1 = (1/2)*(R1[i] + R1[i - 1])*(results.t[i] - results.t[i - 1])
+        MTTF_array1.append(MTTF_elem1)
+
+#Sum all the numerically-integrated segments for the total MTTF
+MTTF1 = sum(MTTF_array1)/365
+print("The mean time to failure is %.2f years.\n" % MTTF1)
+
 relfig = plt.figure(2)
 plt.plot(results.t,R)
 plt.grid(True)
 plt.xlabel('Time (days)')
-plt.ylabel('Prob of system failure')
+plt.ylabel('Probability that System is Functioning')
 plt.title("System Reliability")
 
 plt.show()
