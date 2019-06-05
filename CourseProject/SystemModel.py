712a7ce06a7bef89aa3d807b106a5daa80c28a64
Lane Smith <lanesmith218@gmail.com>
2019-06-05 16:09:59 -0700

    Added MTTF function and a .csv for a low-quality circuit breaker
    Added a MTTF function in the System Model program; it's easier to run now and cleans up the code.
    
    -I also added a Q-matrix for a lower-quality circuit breaker in a .csv file. I simply adjusted the transition rates between the degradation rates. I figured we could do a case study to chow what happens when you unknowingly have a faulty or lower-quality circuit breaker in the system.

--- CourseProject/SystemModel.py
+++ CourseProject/SystemModel.py
@@ -14,6 +14,17 @@ import matplotlib.pyplot as plt
 from CourseProject.ComponentModel import stateProb, addPlots
 from CourseProject.sys_config import *
 
+def MTTF_calc(R, t):
+    MTTF_array = []
+    for i in range(len(R)):
+        if i > 0:
+            #Compute MTTF using numerical integration (trapezoidal rule) for each segment
+            MTTF_elem = (1/2)*(R[i] + R[i - 1])*(t[i] - t[i - 1])
+            MTTF_array.append(MTTF_elem)
+    
+    #Sum all the numerically-integrated segments for the total MTTF
+    MTTF = sum(MTTF_array)/365
+
 
 ##reliability function
 #try 3 components in parallel + 1 in series
@@ -21,8 +32,8 @@ from CourseProject.sys_config import *
 # for comp in range(4):
 #     P[comp] = results.y[3] #prob that device is in fail state (consider including other states like maintenance?)
 
-Pxfmr = stateProb(Xfmr_Qfile,NEW_XFMR,eval_times,start_t=SIM_START,end_t=SIM_END)
-Pcbkr = stateProb(Cbkr_Qfile,NEW_CBKR,eval_times,start_t=SIM_START,end_t=SIM_END)
+Pxfmr = stateProb(Xfmr_Qfile, NEW_XFMR, eval_times, start_t=SIM_START, end_t=SIM_END)
+Pcbkr = stateProb(Cbkr_Qfile, NEW_CBKR, eval_times, start_t=SIM_START, end_t=SIM_END)
 
 #Plots of component probabilities
 addPlots(Pxfmr,1,"Transformer") 
@@ -31,38 +42,26 @@ addPlots(Pcbkr,2,"Circuit Breaker")
 
 P = [Pxfmr.y[3]]*1 + [Pcbkr.y[3]]*3 #failure probs of xfmr in series with 3 parallel cbks
 results = Pxfmr ##just to test
-#make sure this is elementwise multiplication
-R = (1-P[0])*(1-(P[1])*(P[2])*(P[3])) #from Ross Reliability chapter, p(working)=1-P(fail) for each component 
+
+#Reliability functions; from Ross Reliability chapter, p(working)=1-P(fail) for each component 
+R = (1 - P[0])*(1 - P[1]*P[2]*P[3]) #full system
+R1 = (1 - P[0]) #single transformer
+R2 = (1 - P[1]) #single circuit breaker
 
 #Determine the mean time to failure (MTTF) of the system
-MTTF_array = []
-for i in range(len(R)):
-    if i > 0:
-        #Compute MTTF using numerical integration (trapezoidal rule) for each segment
-        MTTF_elem = (1/2)*(R[i] + R[i - 1])*(results.t[i] - results.t[i - 1])
-        MTTF_array.append(MTTF_elem)
-
-#Sum all the numerically-integrated segments for the total MTTF
-MTTF = sum(MTTF_array)/365
+MTTF = MTTF_calc(R, results.t)
 print("The system mean time to failure is %.2f years.\n" % MTTF)
 
-#Reliability of one component
-R1 = (1 - P[0])
+#Determine the mean time to failure (MTTF) of a single transformer
+MTTF1 = MTTF_calc(R1, results.t)
+print("The transformer mean time to failure is %.2f years.\n" % MTTF1)
 
-#Determine the mean time to failure (MTTF) of a single element
-MTTF_array1 = []
-for i in range(len(R1)):
-    if i > 0:
-        #Compute MTTF using numerical integration (trapezoidal rule) for each segment
-        MTTF_elem1 = (1/2)*(R1[i] + R1[i - 1])*(results.t[i] - results.t[i - 1])
-        MTTF_array1.append(MTTF_elem1)
-
-#Sum all the numerically-integrated segments for the total MTTF
-MTTF1 = sum(MTTF_array1)/365
-print("The component mean time to failure is %.2f years.\n" % MTTF1)
+#Determine the mean time to failure (MTTF) of a single circuit breaker
+MTTF2 = MTTF_calc(R2, results.t)
+print("The circuit breaker mean time to failure is %.2f years.\n" % MTTF2)
 
 relfig = plt.figure(3) #fig num
-plt.plot(results.t,R)
+plt.plot(results.t, R)
 plt.grid(True)
 plt.xlabel('Time (days)')
 plt.ylabel('Probability that System is Functioning')
